fe7c0ae6206d0c46c292ccc23bb385ebdd47cb7f
Masamitsu MURASE <masamitsu.murase@gmail.com>
2011-08-03 02:08:31 +0900

    Add readme.rdoc. Refactoring.

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_prevent_multiple_submission do
   name 'Redmine Prevent Multiple Submission plugin'
   author 'Masamitsu MURASE'
   description 'This plugin prevents multiple submission.'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  version '0.0.2'
+  url 'https://github.com/masamitsu-murase/redmine_prevent_multiple_submission/'
+  author_url 'https://github.com/masamitsu-murase/'
 end
--- lib/prevent_multiple_submission_hooks.rb
+++ lib/prevent_multiple_submission_hooks.rb
@@ -2,23 +2,17 @@
 module PreventMultipleSubmission
   class Hooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context = {})
+      pattern = {
+        "issues" => [ "new", "create", "show", "update" ],
+        "issue_moves" => [ "new" ]
+      }
+
       params = context[:controller].params
-
-      case(params[:controller])
-      when "issues"
-        case(params[:action])
-        when "new", "show"
-          return javascript_include_tag("prevent_multiple_submission_issues",
-                                        :plugin => "redmine_prevent_multiple_submission")
-        end
-
-      when "issue_moves"
-        case(params[:action])
-        when "new"
-          return javascript_include_tag("prevent_multiple_submission_issues",
-                                        :plugin => "redmine_prevent_multiple_submission")
-        end
+      if (pattern.key?(params[:controller]) && pattern[params[:controller]].include?(params[:action]))
+        return javascript_include_tag("prevent_multiple_submission_issues",
+                                      :plugin => "redmine_prevent_multiple_submission")
       end
+
       return ""
     end
   end
