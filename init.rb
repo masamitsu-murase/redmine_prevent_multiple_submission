245196588a56bd60b74f195f54e15649a60cc031
Masamitsu MURASE <masamitsu.murase@gmail.com>
2012-02-24 23:08:35 +0900

    Version 0.0.3

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_prevent_multiple_submission do
   name 'Redmine Prevent Multiple Submission plugin'
   author 'Masamitsu MURASE'
   description 'This plugin prevents multiple submission.'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/masamitsu-murase/redmine_prevent_multiple_submission/'
   author_url 'https://github.com/masamitsu-murase/'
 end
